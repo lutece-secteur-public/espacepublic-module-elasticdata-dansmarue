2bda97c3fbdfebc49b6e562e5e81375e9dcf0307
mariejoseph <marie.c.joseph@accenture.com>
2024-03-12 15:16:18 +0100

    BDAT-338

--- src/java/fr/paris/lutece/plugins/elasticdata/modules/dansmarue/business/SignalementDAO.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/dansmarue/business/SignalementDAO.java
@@ -54,9 +54,18 @@ public class SignalementDAO
 
     private static final String SQL_QUERY_SELECTALL = "SELECT ss.id_signalement, se.numero, priorite, type_signalement, alias, alias_mobile, direction, quartier, adresse, coord_x, coord_y, "
             + " arrondissement, secteur, se.date_creation, heure_creation, etat, mail_usager, commentaire_usager, nb_photos, raisons_rejet, "
-            + " nb_suivis, nb_felicitations, date_cloture, is_photo_service_fait, mail_destinataire_courriel, se.courriel_expediteur, date_envoi_courriel, "
-            + " id_mail_service_fait, executeur_service_fait, date_derniere_action, date_prevu_traitement, se.commentaire_agent_terrain, executeur_rejet, "
-            + " executeur_mise_surveillance, nb_requalifications, to_char(ss.service_fait_date_passage,'HH24:MI') heure_sf, executeur_requalification, executeur_requalification_bis, premier_id_type_signalement, premier_direction, se.precision_terrain FROM signalement_export se join signalement_signalement ss on ss.id_signalement = se.id_signalement";
+            + " nb_suivis, nb_felicitations, se.date_cloture, is_photo_service_fait, mail_destinataire_courriel, se.courriel_expediteur, date_envoi_courriel, "
+            + " id_mail_service_fait, executeur_service_fait, se.date_derniere_action, date_prevu_traitement, se.commentaire_agent_terrain, executeur_rejet, "
+            + " executeur_mise_surveillance, nb_requalifications, to_char(ss.service_fait_date_passage,'HH24:MI') heure_sf, executeur_requalification, executeur_requalification_bis, premier_id_type_signalement, premier_direction, se.precision_terrain, "
+            + " TO_CHAR(ss.date_creation, 'dd/mm/yyyy ; HH24:MI:SS') as ts_creation, "
+            + " TO_CHAR(dateHistory.date_cloture, 'dd/mm/yyyy ; HH24:MI:SS') as ts_date_closure, "
+            + " TO_CHAR(lastAction.date_derniere_action, 'dd/mm/yyyy ; HH24:MI:SS') as ts_date_derniere_action, "
+            + " TO_CHAR(ss.date_prevue_traitement, 'dd/mm/yyyy ; HH24:MI:SS') as ts_prevue_traitement, "
+            + " TO_CHAR(ss.courriel_date, 'dd/mm/yyyy ; HH24:MI:SS') as ts_courriel_date, "
+            + " TO_CHAR(ss.service_fait_date_passage, 'dd/mm/yyyy ; HH24:MI:SS') as ts_service_fait_date_passage "
+            + " FROM signalement_export se join signalement_signalement ss on ss.id_signalement = se.id_signalement "
+            + " left join (select id_resource , max(creation_date) as date_derniere_action from workflow_resource_history group by id_resource) as lastAction on ss.id_signalement = lastAction.id_resource"
+            + " left join  (select ss.id_signalement, case when id_state=10 THEN  ss.service_fait_date_passage when id_state=22 THEN  ss.date_mise_surveillance when id_state=11 THEN  ss.date_rejet end date_cloture from signalement_signalement ss left join workflow_resource_history wrh on ss.id_signalement = wrh.id_resource left join workflow_resource_workflow wrw on ss.id_signalement = wrw.id_resource ) as dateHistory on ss.id_signalement =  dateHistory.id_signalement ";
 
     private static final String SQL_QUERY_SELECTALL_WHERE_CLAUSE =" WHERE se.id_signalement IN ({0})";
 
